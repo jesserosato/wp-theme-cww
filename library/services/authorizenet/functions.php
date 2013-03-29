4591dccb4f7240c4bb14ad586023d6d615684bcc
jesserosato <jesse.rosato@gmail.com>
2013-03-28 21:03:29 -0700

    A Striking child theme for a nonprofit organization.

--- library/services/authorizenet/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-add_action('cww_df_pre_process', 'cww_df_authorizenet_pre_process');
-// Use sandbox mode if WP_DEBUG is on.
-function cww_df_authorizenet_pre_process( )
-{
-	defined('WP_DEBUG') && WP_DEBUG ? define('AUTHORIZENET_SANDBOX', true) : define('AUTHORIZENET_SANDBOX', false);
-}
